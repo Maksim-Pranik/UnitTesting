cbd6b876211271ec82f26aa82684c1252140bdaf
Maksim-Pranik <82829188+Maksim-Pranik@users.noreply.github.com>
2021-07-20 11:02:01 +0300

    Changed naming and deleted befor-after Each

--- tests/specs/add.spec.js
+++ tests/specs/add.spec.js
@@ -1,18 +1,15 @@
 const Calculator = require('../../app/calculator');
 const { expect } = require('chai');
 
+const calculator = new Calculator();
+
 describe('function add', function () {
-  let summ;
-  beforeEach(function () {
-    summ = new Calculator();
+  it('should return 9 when operators are 4 and 5', function () {
+    expect(calculator.add(4, 5)).to.be.equal(9);
   });
-  afterEach(function () {
-    summ = null;
-  });
-  it('should return 9 when orerators are 4 and 5', function () {
-    expect(summ.add(4, 5)).to.be.equal(9);
-  });
-  it('should return 1 when orerators are -5 and 6', function () {
-    expect(summ.add(-5, 6)).to.be.equal(1);
+
+  it('should return 1 when operators are -5 and 6', function () {
+    expect(calculator.add(-5, 6)).to.be.equal(1);
   });
 });
+
