89566650e971346cec214010d6701507759859f8
Maksim-Pranik <82829188+Maksim-Pranik@users.noreply.github.com>
2021-07-20 11:03:10 +0300

    Changed naming and deleted befor-after Each

--- tests/specs/multiply.spec.js
+++ tests/specs/multiply.spec.js
@@ -1,21 +1,17 @@
-const Calculator = require('../../app/calculator');
 const { expect } = require('chai');
 
+const calculator = new Calculator();
+
 describe('function multiply', function () {
-  let product;
-  beforeEach(function () {
-    product = new Calculator();
+  it('should return 20 when operators are 4 and 5', function () {
+    expect(calculator.multiply(4, 5)).to.be.equal(20);
   });
-  afterEach(function () {
-    product = null;
+
+  it('should return -30 when operators are -5 and 6', function () {
+    expect(calculator.multiply(-5, 6)).to.be.equal(-30);
   });
-  it('should return 20 when orerators are 4 and 5', function () {
-    expect(product.multiply(4, 5)).to.be.equal(20);
-  });
-  it('should return -30 when orerators are -5 and 6', function () {
-    expect(product.multiply(-5, 6)).to.be.equal(-30);
-  });
-  it('should return 0 when orerators are 0 and 6', function () {
-    expect(product.multiply(0, 6)).to.be.equal(0);
+
+  it('should return 0 when operators are 0 and 6', function () {
+    expect(calculator.multiply(0, 6)).to.be.equal(0);
   });
 });
